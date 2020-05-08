489c0353be5f49696a64a7797717019aa019fad3
Parson, Robert <wrb176@mocs.utc.edu>
2020-05-08 11:09:01 -0400

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegexExpansionMetabot;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine(Capture.Regextract(@"^[\d]+", "0000448-11-7-2019.xlsx"));
+        }
+    }
+}
